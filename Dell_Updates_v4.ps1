dad905df807b5efe738c46c061fab5f78cf3a12f
razo559 <47169393+razo559@users.noreply.github.com>
2023-09-29 10:36:17 -0400

    Dell_Updates_v4.ps1

--- Dell_Updates_v4.ps1
+++ Dell_Updates_v4.ps1
@@ -133,7 +133,7 @@ Get-Service -name 'DellClientManagementService' | Stop-Service -Verbose
 Get-ChildItem -Path "C:\ProgramData\Dell\UpdateService" -Recurse | Remove-Item -Verbose -Confirm:$false
 debugmsg "Starting Patchprocess silently. Logging into $env:Temp\Dell_Command_Update_Patchlogs_$(get-date -f yyyy.MM.dd_H-m)"
 $DCU_category = "firmware,driver"  # bios,firmware,driver,application,others
-$DCUPatching=Start-Process $DellCommandUpdateExePath -ArgumentList "/applyUpdates -autoSuspendBitLocker=enable -reboot=disable -updateType=$DCU_category -outputLog=$env:windir\temp\DCU_Patchlogs_$(get-date -f yyyy.MM.dd_H-m).log" -Wait -Passthru -Verbose
+$DCUPatching=Start-Process $DellCommandUpdateExePath -ArgumentList "/applyUpdates -autoSuspendBitLocker=enable -reboot=disable -updateType=$DCU_category -outputLog=c:\tmp\DCU_Patchlogs_$(get-date -f yyyy.MM.dd_H-m).log" -Wait -Passthru -Verbose
 
 # Interpret the returncode of patching-process:
 switch ( $DCUPatching.ExitCode ) {
