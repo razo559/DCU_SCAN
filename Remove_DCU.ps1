f608574ab1cd61fa7e3577e3d9a7524e51d36ae5
razo559 <47169393+razo559@users.noreply.github.com>
2024-04-25 08:59:44 -0400

    Remove_DCU.ps1

--- Remove_DCU.ps1
+++ Remove_DCU.ps1
@@ -2,7 +2,7 @@
 $Name = "Dell Command*"
 $ProcName = "*Dell*"
 $Timestamp = Get-Date -Format "yyyy-MM-dd_THHmmss" -Verbose
-$LogFile = "$env:windir\temp\DCU-Uninst_$Timestamp.log"
+$LogFile = "c:\tmp\Dell_Command_Temp\DCU-Uninst_$Timestamp.log"
 $ProgramList = @( "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" )
 $Programs = Get-ItemProperty $ProgramList -EA 0 -Verbose
 $App = ($Programs | Where-Object { $_.DisplayName -like $Name -and $_.UninstallString -like "*msiexec*" }).PSChildName
